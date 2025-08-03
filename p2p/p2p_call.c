bbc8d9115c302a1bf680f8cd9deb2c34651d4b34
vik <sasivignesh51@gmail.com>
2025-08-03 17:17:50 +0000

    cedit

--- p2p/p2p_call.c
+++ p2p/p2p_call.c
@@ -5,9 +5,23 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct {
+    RtpSession *rtp;
+    srtp_t srtp;
+} RtpUserData;
+
 static int rtp_send(bzrtpContext_t *ctx, const uint8_t *msg, size_t len) {
     RtpSession *s = (RtpSession*)bzrtp_getUserData(ctx);
     return rtp_session_send_with_ts(s, msg, len, 0);
+    RtpUserData *ud = (RtpUserData*)bzrtp_getUserData(ctx);
+    int out_len = (int)len;
+    if (ud->srtp) {
+        uint8_t *buf = (uint8_t*)msg; /* SRTP modifies packet in place */
+        if (srtp_protect(ud->srtp, buf, &out_len) != srtp_err_status_ok)
+            return -1;
+        return rtp_session_send_with_ts(ud->rtp, buf, out_len, 0);
+    }
+    return rtp_session_send_with_ts(ud->rtp, msg, len, 0);
 }
 
 int main(int argc, char **argv) {
@@ -22,10 +36,13 @@ int main(int argc, char **argv) {
     rtp_session_enable_rtcp(rtp,1);
 
     bzrtpContext_t *zctx = bzrtp_createBzrtpContext();
+    RtpUserData ud = {0};
+    ud.rtp = rtp;
     bzrtpCallbacks_t cbs = {0};
     cbs.bzrtp_sendData = rtp_send;
     bzrtp_setCallbacks(zctx,&cbs);
     bzrtp_setUserData(zctx, rtp);
+    bzrtp_setUserData(zctx, &ud);
     bzrtp_initBzrtpContext(zctx,0x1234);
     bzrtp_startChannelEngine(zctx,0x1234);
 
@@ -48,10 +65,37 @@ int main(int argc, char **argv) {
     policy.ssrc.type = ssrc_any_inbound;
     policy.key = secrets.keyAndSalt;
     policy.window_size = 128; policy.next = NULL;
+    srtp_policy_t policy_in, policy_out;
+    srtp_crypto_policy_set_aes_cm_128_hmac_sha1_80(&policy_in.rtp);
+    srtp_crypto_policy_set_aes_cm_128_hmac_sha1_80(&policy_in.rtcp);
+    policy_in.ssrc.type = ssrc_any_inbound;
+    policy_in.key = secrets.keyAndSaltRS;
+    policy_in.window_size = 128;
+    policy_in.allow_repeat_tx = 0;
+    policy_in.next = &policy_out;
+
+    srtp_crypto_policy_set_aes_cm_128_hmac_sha1_80(&policy_out.rtp);
+    srtp_crypto_policy_set_aes_cm_128_hmac_sha1_80(&policy_out.rtcp);
+    policy_out.ssrc.type = ssrc_any_outbound;
+    policy_out.key = secrets.keyAndSaltLS;
+    policy_out.window_size = 128;
+    policy_out.allow_repeat_tx = 0;
+    policy_out.next = NULL;
+
     srtp_t srtp_session;
     srtp_create(&srtp_session, &policy);
+    srtp_create(&srtp_session, &policy_in);
+    ud.srtp = srtp_session;
 
     /* TODO: add ALSA capture/playback and SRTP encrypt/decrypt of RTP payloads */
+    /* TODO: add ALSA capture/playback */
+    mblk_t *mp = rtp_session_recvm_with_ts(rtp,NULL);
+    if (mp) {
+        int len = msgdsize(mp);
+        srtp_unprotect(srtp_session, mp->b_rptr, &len);
+        /* TODO: decode or play payload in mp->b_rptr of length len */
+        freemsg(mp);
+    }
 
     srtp_dealloc(srtp_session);
     bzrtp_destroyBzrtpContext(zctx);
